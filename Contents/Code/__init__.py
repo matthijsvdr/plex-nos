445d84c54b947d21128189ae4dbfa63765e8f41e
Matthijs <Matthijs@mbookpro.fritz.box>
2013-02-01 06:48:01 +0100

    Correct date parsing, using json, refind servicecode

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import re
+import locale
+locale.setlocale(locale.LC_TIME, "nl_NL")
 
-TITLE               = 'Nos'
+PLUGIN_TITLE               = 'Nos'
 BASE_URL            = 'http://nos.nl'
 VIDEO_PAGE          = '%s/video-en-audio/video/pagina' % BASE_URL
 JOURNAAL_BROADCAST  = '%s/video-en-audio/journaal/pagina' % BASE_URL
@@ -18,24 +20,24 @@ ICON_MORE           = 'art-default.png'
 ####################################################################################################
 
 def Start():
-    Plugin.AddPrefixHandler('/video/nos', MainMenu, TITLE, ICON, ART)
+    Plugin.AddPrefixHandler('/video/nos', MainMenu, PLUGIN_TITLE, ICON, ART)
     Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
     Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-    ObjectContainer.title1 = TITLE
+    ObjectContainer.title1 = PLUGIN_TITLE
     ObjectContainer.art = R(ART)
     ObjectContainer.view_group = 'InfoList'
     DirectoryObject.thumb = R(ICON)
     VideoClipObject.thumb = R(ICON)
 
-    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.CacheTime = 300
     HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17'
     HTTP.Headers['Cookie'] = 'site_cookie_consent=yes'
 
 
 ####################################################################################################
-
 def MainMenu():
+
     oc = ObjectContainer()
 
     oc.add(DirectoryObject(key=Callback(VideosNOS, title='Alle videos van vandaag', url=VIDEO_PAGE, urlcode=1), title='Alle videos van vandaag'))
@@ -45,7 +47,6 @@ def MainMenu():
     oc.add(DirectoryObject(key=Callback(VideosNOS, title='Journaaluitzendingen van vanaag', url=JOURNAAL_BROADCAST, urlcode = 1), title='Journaaluitzendingen van vanaag'))
     
     return oc
-
 
 ####################################################################################################
 
@@ -96,7 +97,8 @@ def VideosNOS(title, url, page=1, urlcode=0):
         finalUrl = VideosPupulairDay(url, page)
     elif urlcode == 3:
         finalUrl = VideosPupulairWeek(url, page)
-
+    
+    # dir = MediaContainer(viewGroup='List', title2=sender.itemTitle)
     oc = ObjectContainer(title2=title)
     now = Datetime.Now()
     content = HTML.ElementFromURL(finalUrl)
@@ -109,45 +111,38 @@ def VideosNOS(title, url, page=1, urlcode=0):
         
         part_url = video.xpath('./a')[0].get('href')
 
-        # some videos link to a different website -> if they go there ignore for now
-        Log.Debug('-----------------json test----------------------------')
-        Log.Debug(part_url)
-
         if not 'nieuwsuur' in part_url:
             page_url = BASE_URL+part_url
-
         else:
             page_url = part_url
 
-        json_return = jsonUrl(part_url)
-        
+        json_return = jsonUrl(part_url)        
         video_title = json_return['title']
-        video_summary = json_return['description']
-        video_thumb = json_return['image']
-        video_url = json_return['videofile']
+        # video_summary = json_return['description']
+        # video_thumb = json_return['image']
+        # video_url = json_return['videofile']
                 
-        date = video.xpath('./em')[0].text
-        split = date.split(',')
-        date = split[0]
-        video_date = Datetime.ParseDate(date, '%m %h %Y')
+        #open second page, for full datetime
+        getDate = HTML.ElementFromURL(page_url)
+        date = getDate.xpath('//ul[@class="meta clearfix"]/li')[0].text
+        video_date = Datetime.ParseDate(date, '%A %d %B %Y, %H:%M')
         
         # debugg lines
         Log.Debug('--------------------video info-------------------------')
         Log.Debug('Log Messages:')
         Log.Debug(part_url)
         Log.Debug(video_title)
-        Log.Debug(video_summary)
-        Log.Debug(video_url)
+        # Log.Debug(video_summary)
+        # Log.Debug(video_url)
         Log.Debug(video_date)
-        Log.Debug(video_thumb)
+        # Log.Debug(video_thumb)
         Log.Debug("page_url - " + page_url)
 
-        oc.add(VideoClipObject(url=page_url, title=video_title, summary=video_summary, originally_available_at=video_date, thumb=Callback(GetThumb, url=video_thumb)))
-
+        oc.add(VideoClipObject(url=page_url, title=video_title, originally_available_at=video_date ))
+        # oc.add(VideoClipObject(url=page_url, title=video_title, summary=video_summary, originally_available_at=video_date, thumb=Callback(GetThumb, url=video_thumb)))
     if len(oc) == 0:
         return MessageContainer('Geen video\'s', 'Deze directory bevat geen video\'s')
     else:
-
         if len(content.xpath('//div[@id="paging"]')) > 0:
             oc.add(DirectoryObject(key=Callback(VideosNOS, title='videos', url=url, page=page + 1, urlcode=urlcode), title='Meer ...', thumb=R(ICON_MORE)))
         return oc
