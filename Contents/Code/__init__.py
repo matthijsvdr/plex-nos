fde591895363c7f19ef7ff083ac25a47ed97afe8
sander1 <sander@plexapp.com>
2013-04-16 22:35:11 +0200

    Remove imports, use hosted resources, grab all info from the main page instead of making x extra http requests.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,155 +1,76 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import re
-import locale
-locale.setlocale(locale.LC_TIME, "nl_NL")
-
-PLUGIN_TITLE               = 'Nos'
-BASE_URL            = 'http://nos.nl'
-VIDEO_PAGE          = '%s/video-en-audio/video/pagina' % BASE_URL
-JOURNAAL_BROADCAST  = '%s/video-en-audio/journaal/pagina' % BASE_URL
-
-PAGE                = '%s/%d/%s/%s-%s-%s/'
-
-ART                 = 'art-default.png'
-ICON                = 'icon.png'
-APP_ICON            = 'icon-app.png'
-
-
 ####################################################################################################
-
 def Start():
-    Plugin.AddPrefixHandler('/video/nos', MainMenu, PLUGIN_TITLE, APP_ICON, ART)
-    Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-    ObjectContainer.title1 = PLUGIN_TITLE
-    ObjectContainer.art = R(ART)
-    ObjectContainer.view_group = 'InfoList'
-    DirectoryObject.thumb = R(ICON)
-    VideoClipObject.thumb = R(ICON)
+	ObjectContainer.title1 = 'NOS'
 
-    HTTP.CacheTime = 300
-    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17'
-    HTTP.Headers['Cookie'] = 'site_cookie_consent=yes'
-
+	HTTP.CacheTime = 300
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17'
+	HTTP.Headers['Cookie'] = 'site_cookie_consent=yes'
 
 ####################################################################################################
+@handler('/video/nos', 'NOS')
 def MainMenu():
 
-    oc = ObjectContainer()
+	oc = ObjectContainer(no_cache=True)
 
-    oc.add(DirectoryObject(key=Callback(VideosNOS, title='Alle videos van vandaag', url=VIDEO_PAGE, urlcode=1), title='Alle videos van vandaag'))
-    oc.add(DirectoryObject(key=Callback(VideosNOS, title='Laatste videos', url=BASE_URL + '/video-en-audio', urlcode=0), title='Laatste videos'))
-    oc.add(DirectoryObject(key=Callback(VideosNOS, title='Meest bekeken videos van vandaag', url=VIDEO_PAGE, urlcode=2), title='Meest bekeken videos van vandaag'))
-    oc.add(DirectoryObject(key=Callback(VideosNOS, title='Meest bekeken deze week', url=VIDEO_PAGE, urlcode=3), title='Meest bekeken deze week'))
-    oc.add(DirectoryObject(key=Callback(VideosNOS, title='Journaaluitzendingen van vanaag', url=JOURNAAL_BROADCAST, urlcode = 1), title='Journaaluitzendingen van vanaag'))
-    
-    return oc
+	oc.add(DirectoryObject(
+		key = Callback(Videos, title="Alle video's van vandaag", url='http://nos.nl/video-en-audio/video/pagina/%%d/datum/%s' % Datetime.Now().strftime('%Y-%m-%d')),
+		title = "Alle video's van vandaag"
+	))
+	oc.add(DirectoryObject(
+		key = Callback(Videos, title="Laatste video's", url='http://nos.nl/video-en-audio'),
+		title = "Laatste video's"
+	))
+	oc.add(DirectoryObject(
+		key = Callback(Videos, title="Meest bekeken vandaag", url='http://nos.nl/video-en-audio/video/pagina/%%d/datum/%s/populair/dag/' % Datetime.Now().strftime('%Y-%m-%d')),
+		title = "Meest bekeken vandaag"
+	))
+	oc.add(DirectoryObject(
+		key = Callback(Videos, title="Meest bekeken deze week", url='http://nos.nl/video-en-audio/video/pagina/%%d/datum/%s/populair/week/' % Datetime.Now().strftime('%Y-%m-%d')),
+		title = "Meest bekeken deze week"
+	))
+	oc.add(DirectoryObject(
+		key = Callback(Videos, title="Journaaluitzendingen van vandaag", url='http://nos.nl/video-en-audio/journaal/pagina/%%d/datum/%s/' % Datetime.Now().strftime('%Y-%m-%d')),
+		title = "Journaaluitzendingen van vandaag"
+	))
 
-####################################################################################################
-
-def DateUrl(url, page=1):
-    now = Datetime.Now()
-    finalUrl = PAGE % (
-        url,
-        page,
-        'datum',
-        now.strftime('%Y'),
-        now.strftime('%m'),
-        now.strftime('%d'),
-        )
-    return finalUrl
-
-def VideosPupulairDay(url, page=1):
-    return DateUrl(url) + 'populair/dag/'
-
-
-def VideosPupulairWeek(url, page=1):
-    return DateUrl(url) + 'populair/week/'
-
-def getJSON(part_url):
-    if ('nieuwsuur' in part_url) or ('koningshuis' in part_url):
-        video_id = part_url.split('/video/')[1].split('-')[0]
-    elif 'uitzendingen' in part_url:
-        video_id = part_url.split('/uitzendingen/')[1].split('-')[0]
-        return JSON.ObjectFromURL("http://nos.nl/playlist/uitzending/mp4-web03/"+video_id+".json")
-    else:
-        video_id = part_url.split('/video/')[1].split('-')[0]
-    return JSON.ObjectFromURL("http://nos.nl/playlist/video/mp4-web03/"+video_id+".json")
+	return oc
 
 ####################################################################################################
+@route('/video/nos/videos', page=int, allow_sync=True)
+def Videos(title, url, page=1):
 
-def VideosNOS(title, url, page=1, urlcode=0):
+	oc = ObjectContainer(title2=title)
+	page_url = url % page if '%d' in url else url
+	content = HTML.ElementFromURL(page_url)
 
-    if urlcode == 0:
-        finalUrl = url
-    elif urlcode == 1:
-        finalUrl = DateUrl(url, page)
-    elif urlcode == 2:
-        finalUrl = VideosPupulairDay(url, page)
-    elif urlcode == 3:
-        finalUrl = VideosPupulairWeek(url, page)
+	for video in content.xpath('//ul[contains(@class, "img-list")]/li'):
+		video_url = video.xpath('./a/@href')[0]
 
-    oc = ObjectContainer(title2=title)
-    content = HTML.ElementFromURL(finalUrl)
+		if not video_url.startswith('http://'):
+			video_url = 'http://nos.nl/%s' % video_url.lstrip('/')
 
-    for video in content.xpath('//ul[@class="img-list equalheight clearfix"]/li'):
-        
-        part_url = video.xpath('./a')[0].get('href')
+		video_title = video.xpath('./a/text()')[0].strip()
+		summary = video.xpath('./span[@class="cat"]/following-sibling::text()')[0].strip()
+		thumb = video.xpath('.//img/@src')[0].replace('/xs/', '/xl/')
+		date = video.xpath('./em/text()')[-1].split(',')[0].replace('mrt', 'mar').replace('mei', 'may').replace('okt', 'oct')
+		date = Datetime.ParseDate(date).date()
 
-        if not 'nieuwsuur' in part_url:
-            page_url = BASE_URL+part_url
-        else:
-            page_url = part_url
+		oc.add(VideoClipObject(
+			url = video_url,
+			title = video_title,
+			summary = summary,
+			thumb = Resource.ContentsOfURLWithFallback(thumb),
+			originally_available_at = date
+		))
 
-        json_return = getJSON(part_url)        
-        video_title = json_return['title']
-        video_summary = json_return['description']
-        video_thumb = json_return['image']
-        video_url = json_return['videofile']
-                
-        #open second page, for full datetime
-        getDate = HTML.ElementFromURL(page_url)
-        if not 'op3' in page_url:
-            date = getDate.xpath('//ul[@class="meta clearfix"]/li')[0].text
-            video_date = Datetime.ParseDate(date, '%A %d %B %Y, %H:%M')
-        else:
-            date = getDate.xpath('//ul[@class="video-meta"]/li')[0].text
-            video_date = Datetime.ParseDate(date, '%d %b %Y, %H:%M')
+	if len(oc) < 1:
+		return ObjectContainer(header="Geen video's", message="Deze directory bevat geen video's")
 
-        
-        # debugg lines
-        Log.Debug('--------------------Log Messages: video info-------------------------')
-        Log.Debug(part_url)
-        Log.Debug(video_title)
-        Log.Debug(video_summary)
-        Log.Debug(video_url)
-        Log.Debug(video_date)
-        Log.Debug(video_thumb)
-        Log.Debug("page_url - " + page_url)
+	if len(content.xpath('//div[@id="paging"]//a[contains(text(), "Volgende")]')) > 0:
+		oc.add(NextPageObject(
+			key = Callback(Videos, title=title, url=url, page=page+1),
+			title = 'Meer ...'
+		))
 
-        oc.add(VideoClipObject(
-            url=page_url, 
-            title=video_title, 
-            summary=video_summary, 
-            originally_available_at=video_date,
-            thumb=Callback(GetThumb, url=video_thumb)))
-
-
-    if len(oc) == 0:
-        return MessageContainer('Geen video\'s', 'Deze directory bevat geen video\'s')
-    else:
-        if len(content.xpath('//div[@id="paging"]')) > 0:
-            oc.add(DirectoryObject(key=Callback(VideosNOS, title=title, url=url, page=page + 1, urlcode=urlcode), title='Meer ...', thumb=R(ICON)))
-        return oc
-
-####################################################################################################
-
-def GetThumb(url):
-    try:
-        image = HTTP.Request(url, cacheTime=CACHE_1WEEK).content
-        return DataObject(image, 'image/jpeg')
-    except:
-        return Redirect(R(ICON))
+	return oc
