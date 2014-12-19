3d014c8676b1e8ea7936b87f4b179f845d8620d9
Sander <sander@plex.tv>
2014-12-19 19:19:34 +0100

    Remove empty VideoClipObject attributes. Update UA.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,13 +4,15 @@ def Start():
 	ObjectContainer.title1 = 'NOS'
 
 	HTTP.CacheTime = 300
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'
 	HTTP.Headers['Cookie'] = 'npo_cc=30'
 
 ####################################################################################################
 @handler('/video/nos', 'NOS')
 def MainMenu():
+
 	oc = ObjectContainer()
+
 	oc.add(DirectoryObject(
 		key = Callback(Videos, title="Laatste journaaluitzendingen", url='http://nos.nl/uitzending/nos-journaal'),
 		title = "Laatste journaaluitzendingen"
@@ -51,33 +53,30 @@ def MainMenu():
 def Videos(title, url, page=1):
 
 	oc = ObjectContainer(title2=title)
-	page_url = url % page if '%d' in url else url
+	page_url = url % (page) if '%d' in url else url
 	content = HTML.ElementFromURL(page_url)
 	rbar = content.xpath('.//div[contains(@class, "broadcast-player__playlist")]')[0]
+
 	for video in rbar.xpath('.//a[contains(@class, "broadcast-link")]'):
+
 		video_url = video.xpath('./@href')[0]
 
 		if not video_url.startswith('http://'):
 			video_url = 'http://nos.nl/%s' % video_url.lstrip('/')
 
 		video_title = video.xpath('./span[contains(@class, "broadcast-link__name")]/text()')[0].strip() + ' ' + video.xpath('./time[contains(@class, "broadcast-link__date")]/text()')[0].strip()
-		summary = '' #video.xpath('./span[@class="cat"]/following-sibling::text()')[0].strip()
-		thumb = '' #video.xpath('.//img/@src')[0].replace('/xs/', '/xl/')
-		date = '' #video.xpath('./em/text()')[-1].split(',')[0].replace('mrt', 'mar').replace('mei', 'may').replace('okt', 'oct')
-		date = Datetime.ParseDate('2014/01/01').date()
 
 		oc.add(VideoClipObject(
 			url = video_url,
 			title = video_title,
-			summary = summary,
-			thumb = Resource.ContentsOfURLWithFallback(thumb),
-			originally_available_at = date
+			thumb = Resource.ContentsOfURLWithFallback(url='')
 		))
 
 	if len(oc) < 1:
 		return ObjectContainer(header="Geen video's", message="Deze directory bevat geen video's")
 
 	if len(content.xpath('//div[@id="paging"]//a[contains(text(), "Volgende")]')) > 0:
+
 		oc.add(NextPageObject(
 			key = Callback(Videos, title=title, url=url, page=page+1),
 			title = 'Meer ...'
